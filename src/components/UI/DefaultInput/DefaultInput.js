561c8ffcad38e35902c6c5216bb5dcc306b4d0b7
kminny <darkdevil94@naver.com>
2019-07-07 17:49:06 +0900

    Background image

--- src/components/UI/DefaultInput/DefaultInput.js
+++ src/components/UI/DefaultInput/DefaultInput.js
@@ -3,17 +3,17 @@ import { TextInput, StyleSheet } from "react-native";
 
 const defaultInput = props => (
   <TextInput
-    style={styles.input}
     underlineColorAndroid="transparent"
     {...props}
+    style={[styles.input, props.style]}
   />
 );
 
 const styles = StyleSheet.create({
   input: {
     width: "100%",
-    borderBottomColor: "#eee",
-    borderBottomWidth: 1,
+    borderColor: "#eee",
+    borderWidth: 1,
     padding: 5,
     margin: 8
   }
