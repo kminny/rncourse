6c4c0d77f1b4803843b29b621312f070fc91c904
kminny <darkdevil94@naver.com>
2019-07-12 17:20:32 +0900

    React-Native-Map installed

--- android/app/src/main/java/com/rncourse/MainApplication.java
+++ android/app/src/main/java/com/rncourse/MainApplication.java
@@ -9,8 +9,8 @@ import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import com.oblador.vectoricons.VectorIconsPackage;
-
 import com.reactnativenavigation.NavigationApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -51,13 +51,15 @@ public class MainApplication extends NavigationApplication  {
 	public boolean isDebug() {
 		// Make sure you are using BuildConfig from your own application
 		return BuildConfig.DEBUG;
-	}
-
+  }
+  
 	protected List<ReactPackage> getPackages() {
 		// Add additional packages you require here
 		// No need to add RnnPackage and MainReactPackage
 		return Arrays.<ReactPackage>asList(
-      new VectorIconsPackage()
+      new VectorIconsPackage(),
+      new MainReactPackage(),
+      new MapsPackage()
 		);
 	}
 
